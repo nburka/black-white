f09a7d6392edaa65b130b0056f178753c09caa24
Nick Burka <nick@silverorange.com>
2015-01-03 12:29:16 -0500

    More work with back links and black background

--- www/black-white-meteor.js
+++ www/black-white-meteor.js
@@ -2,12 +2,22 @@ if (Meteor.isClient) {
 	Template.registerHelper('dateFormatLong', function(date) {
 		return moment(date).format('MMMM Do YYYY');
 	});
+
 	Template.registerHelper('dateFormat', function(date, format) {
 		return moment(date).format(format);
 	});
+
 	Template.registerHelper('setPageTitle', function() {
 		var args = _.toArray(arguments);
 		document.title = args.slice(0, args.length - 1).join(' | ');
+	});
+
+	Template.registerHelper('addBodyClass', function(classes) {
+		$(document.body).addClass(classes);
+	});
+
+	Template.registerHelper('removeBodyClass', function(classes) {
+		$(document.body).removeClass(classes);
 	});
 }
 
