6372e26cff4291d1a0cb6ca3089bfe51eb384ec3
Nick Burka <nick@silverorange.com>
2015-01-06 23:52:13 -0500

    Much better, cleaned-up router

--- www/lib/router.js
+++ www/lib/router.js
@@ -22,94 +22,78 @@ Router.configure({
 
 Router.plugin('dataNotFound', {notFoundTemplate: 'notFoundPage'});
 
-Router.route('/', function() {
-	this.render('projectsPage');
-}, { name: 'projectsPage', fastRender: true });
+Router.route(
+	'/',
+	{
+		name: 'projectsPage',
+		path: '/',
+		fastRender: true
+	}
+);
 
-Router.route('/feed', function() {
-	var request = this.request;
+Router.route(
+	'/photos',
+	{
+		name: 'photosPage',
+		fastRender: true
+	}
+);
 
-	var rssNpm = Meteor.npmRequire('rss');
-
-	var feed = new rssNpm({
-		title: Meteor.settings.public.site.title,
-		description: 'Photos by Nicholas Burka',
-		feed_url: Meteor.absoluteUrl('feed'),
-		site_url: Meteor.absoluteUrl(),
-		copyright: _.str.sprintf(
-			'All rights reserved %s, Nicholas Burka',
-			moment().format('YYYY')
-		),
-		categories: ['Photography']
-	});
-
-	var photos = Photos.find(
-		{},
-		{
-			sort: { publish_date: -1, _id: -1},
-			limit: 50
-		}
-	);
-
-	photos.forEach(function(photo) {
-		var img_tag = _.str.sprintf(
-			'<img src="%s/photos/1000/%s" />',
-			Meteor.settings.public.uri.cdn,
-			photo.filename
-		);
-
-		feed.item({
-			title: photo.title,
-			description: img_tag,
-			url: Meteor.absoluteUrl('photos/' + photo._id),
-			date: photo.publish_date,
-			enclosure: { file: Meteor.absoluteUrl('photos/' + photo._id + '.jpg') },
-		});
-	});
-
-	this.response.setHeader('content-type', 'application/rss+xml; charset=utf-8');
-	this.response.end(feed.xml({indent: true}));
-}, { where: 'server' });
-
-Router.route('/photos', function() {
-	this.render('photosPage');
-}, { name: 'photosPage' });
-
-Router.route('/photos/:_id', function() {
-	this.render('photoPage', {
+Router.route(
+	'/photos/:_id',
+	{
+		name: 'photoPage',
 		data: function() {
 			return Photos.findOne({_id: this.params._id});
-		}
-	});
-}, {
-	name: 'photoPage',
-	layoutTemplate: '',
-	fastRender: true
-});
+		},
+		layoutTemplate: '',
+		fastRender: true
+	}
+);
 
-Router.route('/projects', function() {
-	this.redirect('/');
-}, { name: 'projects-redirect' });
+Router.route(
+	'/projects',
+	function() {
+		this.redirect('/');
+	}
+);
 
-Router.route('/projects/:_id', function() {
-	this.render('projectPage', {
+Router.route(
+	'/projects/:_id',
+	{
+		name: 'projectPage',
 		data: function() {
 			return Projects.findOne({_id: this.params._id});
-		}
-	});
-}, { name: 'projectPage', fastRender: true });
+		},
+		fastRender: true
+	}
+);
 
-Router.route('projects/:project_id/photos/:_id', function() {
-	this.render('projectPhotoPage', {
+Router.route(
+	'projects/:project_id/photos/:_id',
+	{
+		name: 'projectPhotoPage',
+		layoutTemplate: '',
 		data: function() {
 			return {
 				photo: Photos.findOne({_id: this.params._id}),
 				project: Projects.findOne({_id: this.params.project_id})
 			}
-		}
-	});
-}, {
-	name: 'projectPhotoPage',
-	layoutTemplate: '',
-	fastRender: true
-});
+		},
+		fastRender: true
+	}
+);
+
+// server routes
+Router.route(
+	'/feed',
+	function() {
+		var request = this.request;
+		var feed = new PhotosRssFeed();
+		this.response.setHeader('content-type', 'application/rss+xml; charset=utf-8');
+		this.response.end(feed.xml({indent: true}));
+	},
+	{
+		where: 'server'
+	}
+);
